c98f93795be19f38979dd059f3312f126ae00100
Clemens Banowski <officialeta09@gmail.com>
2024-04-10 15:59:39 +0200

    improved speed and user checking

--- app/scrape.py
+++ app/scrape.py
@@ -1,5 +1,6 @@
 # scrape.py {api_id} {api_hash} {phone} {from_group} {final_group} {bot_id}
 
+import re
 import sys
 import time
 import random
@@ -27,13 +28,13 @@ time.sleep(1)
 users = client.get_participants(from_group, aggressive=True)
 
 for user in users:
-    time.sleep(1)
+    time.sleep(.5)
     try:
-        if not user.is_self and not user.bot and user.username and user.id != int(sys.argv[6]):
+        if not user.is_self and not user.bot and user.username and user.photo is None and re.search(r'[^a-zA-Z0-9äöüÄÖÜß]', user.username) is None and user.id != int(sys.argv[6]):
             user_to_add = client.get_input_entity(user.username)
             print("Adding: {}".format(user.username))
             client(InviteToChannelRequest(final_group_entity, [user_to_add]))
-            time.sleep(random.randrange(5, 10))
+            time.sleep(random.randrange(2, 4))
     except PeerFloodError:
         print("[!] Getting Flood Error from telegram.")
     except UserPrivacyRestrictedError:
@@ -43,4 +44,4 @@ for user in users:
         print("[!] Unexpected Error")
         continue
 
-    time.sleep(1)
+    time.sleep(.5)
