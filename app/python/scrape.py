7f94515f3db1c388c56dc9c031ef6b78e8d75a0b
Clemens Banowski <officialeta09@gmail.com>
2024-05-01 15:25:43 +0200

    check user profile picture and online status

--- app/python/scrape.py
+++ app/python/scrape.py
@@ -10,8 +10,7 @@ import json
 from telethon.sync import TelegramClient
 from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
 from telethon.tl.functions.channels import InviteToChannelRequest
-from telethon.tl.types import InputPeerChannel
-
+from telethon.tl.types import InputPeerChannel, UserStatusOnline, UserStatusRecently, UserProfilePhoto
 
 user_already_added_file = './shared/userAlreadyAdded.txt'
 excepted_user_strings_file = './shared/exceptedUserStrings.txt'
@@ -72,51 +71,52 @@ random.shuffle(users)
 
 
 for user in users:
-    if not user.is_self and not user.bot and not user.fake and not user.support:
+    if not user.is_self and not user.bot and not user.fake and not user.support and not isinstance(user.photo, UserProfilePhoto):
         if user.username and re.search(r'[^a-zA-Z0-9äöüÄÖÜß]', str(user.first_name).lower()) is None and re.search(r'[^a-zA-Z0-9äöüÄÖÜß]', str(user.last_name).lower()) is None:
             if not string_in_array(user.username.lower(), exceptedUserStrings) and not string_in_array(user.first_name.lower(), exceptedUserStrings):
+                if isinstance(user.status, UserStatusOnline) or isinstance(user.status, UserStatusRecently):
 
-                try:
-                    with open(user_already_added_file, 'r+') as file:
-                        alreadyAddedUsers = [alreadyAddedUser.strip() for alreadyAddedUser in file.readlines() if alreadyAddedUser.strip() != ""]
+                    try:
+                        with open(user_already_added_file, 'r+') as file:
+                            alreadyAddedUsers = [alreadyAddedUser.strip() for alreadyAddedUser in file.readlines() if alreadyAddedUser.strip() != ""]
 
-                        if user.username in alreadyAddedUsers:
-                            time.sleep(.2)
+                            if user.username in alreadyAddedUsers:
+                                time.sleep(.2)
 
-                        else:
-                            try:
-                                user_to_add = client.get_input_entity(user.username)
-                                client(InviteToChannelRequest(final_group_entity, [user_to_add]))
-                                file.write('\n' + user.username)
+                            else:
+                                try:
+                                    user_to_add = client.get_input_entity(user.username)
+                                    client(InviteToChannelRequest(final_group_entity, [user_to_add]))
+                                    file.write('\n' + user.username)
 
-                                print_data('ADD_USER', {
-                                    'username': user.username,
-                                    'firstname': user.first_name,
-                                    'lastname': user.last_name
-                                })
-                                time.sleep(random.randrange(40, 60))
+                                    print_data('ADD_USER', {
+                                        'username': user.username,
+                                        'firstname': user.first_name,
+                                        'lastname': user.last_name
+                                    })
+                                    time.sleep(random.randrange(40, 60))
 
-                            except PeerFloodError:
-                                print_data('ERROR', 'USERADD_FLOOD')
-                                time.sleep(random.randrange(1800, 5400))
+                                except PeerFloodError:
+                                    print_data('ERROR', 'USERADD_FLOOD')
+                                    time.sleep(random.randrange(1800, 5400))
 
-                            except UserPrivacyRestrictedError:
-                                file.write('\n' + user.username)
-                                print_data('ERROR', 'USERADD_PRIVACY')
-                                time.sleep(random.randrange(10, 20))
+                                except UserPrivacyRestrictedError:
+                                    file.write('\n' + user.username)
+                                    print_data('ERROR', 'USERADD_PRIVACY')
+                                    time.sleep(random.randrange(10, 20))
 
-                            except:
-                                file.write('\n' + user.username)
-                                print_data('ERROR', 'USERADD_UNKNOWN')
-                                time.sleep(random.randrange(10, 20))
+                                except:
+                                    file.write('\n' + user.username)
+                                    print_data('ERROR', 'USERADD_UNKNOWN')
+                                    time.sleep(random.randrange(10, 20))
 
-                except FileNotFoundError:
-                    print_data('ERROR', 'COULD_NOT_FIND_USERALREADYADDED')
-                    create_user_already_added_file()
-                    time.sleep(1)
+                    except FileNotFoundError:
+                        print_data('ERROR', 'COULD_NOT_FIND_USERALREADYADDED')
+                        create_user_already_added_file()
+                        time.sleep(1)
 
-                except:
-                    print_data('ERROR', 'COULD_NOT_OPEN_USERALREADYADDED')
-                    time.sleep(10)
-                    create_user_already_added_file()
-                    time.sleep(1)
+                    except:
+                        print_data('ERROR', 'COULD_NOT_OPEN_USERALREADYADDED')
+                        time.sleep(10)
+                        create_user_already_added_file()
+                        time.sleep(1)
