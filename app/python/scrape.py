8d903685fbcc6f3cbd1789cd34485c6567c42e0f
Clemens Banowski <officialeta09@gmail.com>
2025-01-24 17:16:06 +0100

    implemented complies_with_user_rules method

--- app/python/scrape.py
+++ app/python/scrape.py
@@ -61,6 +61,22 @@ def is_user_in_group(group, username_to_find):
         return False
 
 
+def complies_with_user_rules(user):
+    return not user.is_self \
+        and not user.bot \
+        and not user.fake \
+        and not user.support \
+        and not user.deleted \
+        and not isinstance(user.photo, UserProfilePhoto) \
+        and not user.username \
+        and len(str(user.first_name)) > 1 \
+        and re.search(r'[^a-zA-Z0-9äöüÄÖÜß]', str(user.first_name).lower()) is None \
+        and re.search(r'[^a-zA-Z0-9äöüÄÖÜß]', str(user.last_name).lower()) is None \
+        and not string_in_array(user.username.lower(), exceptedUserStrings) \
+        and not string_in_array(user.first_name.lower(), exceptedUserStrings) \
+        and (isinstance(user.status, UserStatusOnline) or isinstance(user.status, UserStatusRecently))
+
+
 create_user_already_added_file()
 
 try:
@@ -96,67 +112,59 @@ random.shuffle(users)
 
 
 for user in users:
-    if not user.is_self and not user.bot and not user.fake and not user.support and not user.deleted and not isinstance(
-            user.photo, UserProfilePhoto):
-        if user.username and len(str(user.first_name)) > 1 and re.search(r'[^a-zA-Z0-9äöüÄÖÜß]',
-                                                                         str(user.first_name).lower()) is None and re.search(
-                r'[^a-zA-Z0-9äöüÄÖÜß]', str(user.last_name).lower()) is None:
-            if not string_in_array(user.username.lower(), exceptedUserStrings) and not string_in_array(
-                    user.first_name.lower(), exceptedUserStrings):
-                if isinstance(user.status, UserStatusOnline) or isinstance(user.status, UserStatusRecently):
+    if complies_with_user_rules(user):
+        try:
+            with open(user_already_added_file, 'r+') as file:
+                alreadyAddedUsers = [alreadyAddedUser.strip() for alreadyAddedUser in file.readlines() if
+                                     alreadyAddedUser.strip() != ""]
+
+                if user.username in alreadyAddedUsers:
+                    time.sleep(.2)
+
+                else:
+                    try:
+                        if sys.argv[6] and int(sys.argv[6]) != -1:
+                            participants = client.get_participants(final_group_entity)
+                            if len(participants) >= int(sys.argv[6]):
+                                print_data('MAX_USERS_REACHED', int(sys.argv[6]))
+                                exit()
+                    except:
+                        time.sleep(.2)
+                        continue
 
                     try:
-                        with open(user_already_added_file, 'r+') as file:
-                            alreadyAddedUsers = [alreadyAddedUser.strip() for alreadyAddedUser in file.readlines() if
-                                                 alreadyAddedUser.strip() != ""]
+                        user_to_add = client.get_input_entity(user.username)
+                        client(InviteToChannelRequest(final_group_entity, [user_to_add]))
+                        file.write('\n' + user.username)
 
-                            if user.username in alreadyAddedUsers:
-                                time.sleep(.2)
+                        print_data('ADD_USER', {
+                            'username': user.username,
+                            'firstname': user.first_name,
+                            'lastname': user.last_name
+                        })
+                        time.sleep(random.randrange(40, 60))
 
-                            else:
-                                try:
-                                    if sys.argv[6] and int(sys.argv[6]) != -1:
-                                        participants = client.get_participants(final_group_entity)
-                                        if len(participants) >= int(sys.argv[6]):
-                                            print_data('MAX_USERS_REACHED', int(sys.argv[6]))
-                                            exit()
-                                except:
-                                    time.sleep(.2)
-                                    continue
+                    except PeerFloodError:
+                        print_data('ERROR', 'USERADD_FLOOD')
+                        time.sleep(random.randrange(1800, 5400))
 
-                                try:
-                                    user_to_add = client.get_input_entity(user.username)
-                                    client(InviteToChannelRequest(final_group_entity, [user_to_add]))
-                                    file.write('\n' + user.username)
-
-                                    print_data('ADD_USER', {
-                                        'username': user.username,
-                                        'firstname': user.first_name,
-                                        'lastname': user.last_name
-                                    })
-                                    time.sleep(random.randrange(40, 60))
-
-                                except PeerFloodError:
-                                    print_data('ERROR', 'USERADD_FLOOD')
-                                    time.sleep(random.randrange(1800, 5400))
-
-                                except UserPrivacyRestrictedError:
-                                    file.write('\n' + user.username)
-                                    print_data('ERROR', 'USERADD_PRIVACY')
-                                    time.sleep(random.randrange(10, 20))
-
-                                except:
-                                    file.write('\n' + user.username)
-                                    print_data('ERROR', 'USERADD_UNKNOWN')
-                                    time.sleep(random.randrange(10, 20))
-
-                    except FileNotFoundError:
-                        print_data('ERROR', 'COULD_NOT_FIND_USERALREADYADDED')
-                        create_user_already_added_file()
-                        time.sleep(1)
+                    except UserPrivacyRestrictedError:
+                        file.write('\n' + user.username)
+                        print_data('ERROR', 'USERADD_PRIVACY')
+                        time.sleep(random.randrange(10, 20))
 
                     except:
-                        print_data('ERROR', 'COULD_NOT_OPEN_USERALREADYADDED')
-                        time.sleep(10)
-                        create_user_already_added_file()
-                        time.sleep(1)
+                        file.write('\n' + user.username)
+                        print_data('ERROR', 'USERADD_UNKNOWN')
+                        time.sleep(random.randrange(10, 20))
+
+        except FileNotFoundError:
+            print_data('ERROR', 'COULD_NOT_FIND_USERALREADYADDED')
+            create_user_already_added_file()
+            time.sleep(1)
+
+        except:
+            print_data('ERROR', 'COULD_NOT_OPEN_USERALREADYADDED')
+            time.sleep(10)
+            create_user_already_added_file()
+            time.sleep(1)
